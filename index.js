a88b8809b11f1ab36e1cb7efa5b45b8994a0080a
lyonbot <lyonbot@gmail.com>
2018-02-15 15:55:34 +0800

    避免非微信环境下，多余的resize

--- index.js
+++ index.js
@@ -11,15 +11,18 @@ function iideo(container) {
   var video = story.el
 
   // 微信
-  window.addEventListener('resize', function () {
-    video.style.width = window.innerWidth + "px";
-    video.style.height = window.innerHeight + "px";
-    setTimeout(() => story.updateFitCalc(), 300);
-  }, false)
+  if (/micromessenger/i.test(navigator.userAgent)) {
+    window.addEventListener('resize', function () {
+      video.style.width = window.innerWidth + "px";
+      video.style.height = window.innerHeight + "px";
+     setTimeout(() => story.updateFitCalc(), 300);
+    }, false)
+  }
 
   return { story, video }
 }
 
+iideo.StoryTeller = StoryTeller
 iideo.Button = Button
 
 window.iideo = iideo
